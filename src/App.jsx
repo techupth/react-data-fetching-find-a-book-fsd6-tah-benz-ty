498f89302990e83f7d2fe80c3603b699819c348e
TiTY <tee25450@gmail.com>
2024-06-07 14:46:51 +0700

    feat:search-box

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [data, setData] = useState([]);
+
+  const [search, setSearch] = useState("");
+
+  const getData = async (query) => {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}`
+    );
+    setData(result.data.items);
+  };
+
+  useEffect(() => {
+    getData(search);
+  }, [search]);
+
+  const handleTextChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input id="find-book" type="text" onChange={handleTextChange} />
+      <ul>
+        {data.map((book) => (
+          <li key={book.id}>{book.volumeInfo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
